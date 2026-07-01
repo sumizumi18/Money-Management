a0dff20da5c73906c5568e237aae6e0202dbdf59
sumizumi18 <74195997+sumizumi18@users.noreply.github.com>
2026-07-01 23:05:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mt-v3-fresh";
+const CACHE = "mt-v3-fresh-2";
 const FILES = ["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]));});
